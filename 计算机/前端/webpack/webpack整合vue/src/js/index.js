0a06f910abc86d39ee840bd3762f4e77c73210ce
cgte1987911 <hongfa2011@126.com>
2020-02-23 00:23:00 +0800

    'webpack配置vue'

---
+++ 计算机/前端/webpack/webpack整合vue/src/js/index.js
@@ -0,0 +1,20 @@
+// import "../css/main.css";
+// import "../less/1.less";
+//
+// window.onload=function (){
+//   document.onclick=function (){
+//     //alert("a");
+//   };
+// };
+
+import Vue from 'vue';
+import App from './App.vue';
+
+let vm=new Vue({
+  el: '#root',
+  data: {},
+  template: '<App/>',
+  components: {
+    App
+  }
+});
